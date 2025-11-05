9338d0513594ff77751a832779ee3cf6ba5878c5
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-05 13:28:56 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,2 +1,6 @@
+from ip_tracking.models import RequestLog
+RequestLog.objects.all()
 python manage.py makemigrations ip_tracking
 python manage.py migrate
+python manage.py runserver
+python manage.py shell
