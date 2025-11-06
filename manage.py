49da74294cc8325c5c9e35350cc8ea5e2300a10e
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-06 12:58:19 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 from ip_tracking.models import RequestLog
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alx_backend_security.settings')
 RequestLog.objects.all()
 python manage.py makemigrations ip_tracking
 python manage.py migrate
