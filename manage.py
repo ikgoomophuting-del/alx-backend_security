d5407beb67d8901c1ac3eb0a8b9c03f350daef3c
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-06 13:12:08 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,7 +1,24 @@
 from ip_tracking.models import RequestLog
+#!/usr/bin/env python
+import os
+import sys
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alx_backend_security.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+if __name__ == '__main__':
+    main()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alx_backend_security.settings')
+
 RequestLog.objects.all()
-python manage.py makemigrations ip_tracking
-python manage.py migrate
-python manage.py runserver
-python manage.py shell
+
