124d9a6b8fc205d7b848792aefe6c73b7d8efa49
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-09 23:03:13 +0200

    Update settings.py

--- alx_backend_security/settings.py
+++ alx_backend_security/settings.py
@@ -44,10 +44,14 @@ INSTALLED_APPS = [
 
 ]
 
-'DEFAULT_AUTHENTICATION_CLASSES': [
-    'rest_framework.authentication.SessionAuthentication',
-    'rest_framework.authentication.BasicAuthentication',
-],
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 
 # --- MIDDLEWARE ---
 MIDDLEWARE = [
