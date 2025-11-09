d4192ae2104c35a2b4bc5313bd654a87354e43d6
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-09 23:04:46 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -44,11 +44,16 @@ INSTALLED_APPS = [
 
 ]
 
-'DEFAULT_AUTHENTICATION_CLASSES': [
-    'rest_framework.authentication.SessionAuthentication',
-    'rest_framework.authentication.BasicAuthentication',
-],
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 
+    
 # --- MIDDLEWARE ---
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
