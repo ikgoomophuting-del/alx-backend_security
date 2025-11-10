bfa55de03420300983608d1ac3862cac066df005
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-10 14:22:47 +0200

    Create models.py

---
+++ models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.utils import timezone
+
+
+class RequestLog(models.Model):
+    ip_address = models.GenericIPAddressField()
+    path = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.ip_address} - {self.path} at {self.timestamp}"
