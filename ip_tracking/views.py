5913c470a407c9fd62c7d969050455c0bef50131
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-11-16 02:38:13 +0200

    Update views.py

--- ip_tracking/views.py
+++ ip_tracking/views.py
@@ -1,8 +1,4 @@
 from django.http import JsonResponse
-from ratelimit.decorators import ratelimit
 
-# Anonymous: 5 req/min, Authenticated: 10 req/min
-@ratelimit(key='ip', rate='5/m', block=True)
-@ratelimit(key='user', rate='10/m', block=True)
 def sensitive_view(request):
     return JsonResponse({"message": "Access granted!"})
